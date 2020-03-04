9f609778dbf50d2f90d9959bd745c084ebacab9c
Emilia <Emilia@DESKTOP-D0STBN9.technicolor.net>
2020-03-04 21:59:17 +0100

    Properties added to TransferApplication

--- src/main/java/org/springframework/samples/petclinic/model/TransferApplication.java
+++ src/main/java/org/springframework/samples/petclinic/model/TransferApplication.java
@@ -3,6 +3,9 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import com.sun.istack.NotNull;
 
 import lombok.Data;
 
@@ -11,7 +14,13 @@ import lombok.Data;
 @Table(name = "transfer_applications")
 public class TransferApplication extends BaseEntity {
 
+	@NotNull
+	private Double amount;
+	
 	@NotEmpty
 	private TransferApplicationStatus status;
 
+	@NotEmpty
+	@Pattern(regexp = "^[A-Z]{2}(?:[]?[0-9]){18-20}$", message = "Invalid account number")
+	private String accountNumber;
 }
