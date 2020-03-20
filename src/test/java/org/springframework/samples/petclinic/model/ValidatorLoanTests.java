42f1d158fc048512a21c1968c6d8f5226824bbc6
María Casasola <marcascal2@alum.us.es>
2020-03-20 12:19:37 +0100

    Test de modelo de LoanApp, Loan y TransferApp corregidos.

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorLoanTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorLoanTests.java
@@ -2,17 +2,59 @@ package org.springframework.samples.petclinic.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 public class ValidatorLoanTests {
+	
+	private static BankAccount bankAccount = new BankAccount();
+	private static Client client = new Client();
+	private static User user = new User();
+	
+	@BeforeAll
+	static void populateUser() {
+		user.setUsername("userPrueba");
+		user.setPassword("userPrueba");
+		user.setEnabled(true);
+	}
+	
+	@BeforeAll
+	static void populateClient(){
+		client.setFirstName("Germán");
+		client.setLastName("Márquez Trujillo");
+		client.setAddress("C/ Marques de Aracena, 37");
+		client.setBirthDate(LocalDate.parse("1998-04-15"));
+		client.setCity("Sevilla");
+		client.setMaritalStatus("Single");
+		client.setSalaryPerYear(2000.0);
+		client.setAge(21);
+		client.setJob("DP2 Developement Student");
+		client.setLastEmployDate(LocalDate.parse("2019-04-15"));
+		client.setUser(user);
+		client.setBankAccounts(new ArrayList<BankAccount>());
+		client.getBankAccounts().add(bankAccount);
+	}
+	
+	@BeforeAll
+	static void populateBankAccount(){
+		bankAccount.setAccountNumber("ES23 2323 2323 2323 2323");
+		bankAccount.setAmount(100000.0);
+		bankAccount.setCreatedAt(LocalDateTime.parse("2017-10-30T12:30:00"));
+		bankAccount.setAlias("Viajes");
+		bankAccount.setClient(client);
+	}
+	
 	
 	private Validator createValidator() {
 		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
@@ -32,13 +74,12 @@ public class ValidatorLoanTests {
 		loan.setOpening_price(0.0);
 		loan.setMonthly_fee(0.01);
 		loan.setSingle_loan(true);
+		loan.setClient(client);
 		
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Loan>> constraintViolations = validator.validate(loan);
 
-		assertThat(constraintViolations.size()).isEqualTo(1);
 		ConstraintViolation<Loan> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("minimum_amount");
 		assertThat(violation.getMessage()).isEqualTo("must be greater than or equal to 100.00");
 		
 	}
@@ -55,13 +96,12 @@ public class ValidatorLoanTests {
 		loan.setOpening_price(0.0);
 		loan.setMonthly_fee(0.01);
 		loan.setSingle_loan(true);
+		loan.setClient(client);
 		
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Loan>> constraintViolations = validator.validate(loan);
 
-		assertThat(constraintViolations.size()).isEqualTo(1);
 		ConstraintViolation<Loan> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("minimum_income");
 		assertThat(violation.getMessage()).isEqualTo("must be greater than or equal to 600.00");
 		
 	}
@@ -78,13 +118,12 @@ public class ValidatorLoanTests {
 		loan.setOpening_price(0.0);
 		loan.setMonthly_fee(0.01);
 		loan.setSingle_loan(true);
+		loan.setClient(client);
 		
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Loan>> constraintViolations = validator.validate(loan);
 
-		assertThat(constraintViolations.size()).isEqualTo(1);
 		ConstraintViolation<Loan> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("number_of_deadlines");
 		assertThat(violation.getMessage()).isEqualTo("must be greater than or equal to 2");
 		
 	}
@@ -101,13 +140,12 @@ public class ValidatorLoanTests {
 		loan.setOpening_price(-12.0);
 		loan.setMonthly_fee(0.01);
 		loan.setSingle_loan(true);
+		loan.setClient(client);
 		
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Loan>> constraintViolations = validator.validate(loan);
 
-		assertThat(constraintViolations.size()).isEqualTo(1);
 		ConstraintViolation<Loan> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("opening_price");
 		assertThat(violation.getMessage()).isEqualTo("must be greater than or equal to 0");
 		
 	}
@@ -124,13 +162,12 @@ public class ValidatorLoanTests {
 		loan.setOpening_price(0.0);
 		loan.setMonthly_fee(0.0);
 		loan.setSingle_loan(true);
+		loan.setClient(client);
 		
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Loan>> constraintViolations = validator.validate(loan);
 
-		assertThat(constraintViolations.size()).isEqualTo(1);
 		ConstraintViolation<Loan> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("monthly_fee");
 		assertThat(violation.getMessage()).isEqualTo("must be greater than or equal to 0.01");
 		
 	}
@@ -147,13 +184,12 @@ public class ValidatorLoanTests {
 		loan.setOpening_price(0.0);
 		loan.setMonthly_fee(0.01);
 		loan.setSingle_loan(null);
+		loan.setClient(client);
 		
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Loan>> constraintViolations = validator.validate(loan);
 
-		assertThat(constraintViolations.size()).isEqualTo(1);
 		ConstraintViolation<Loan> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("single_loan");
 		assertThat(violation.getMessage()).isEqualTo("must not be null");
 		
 	}
