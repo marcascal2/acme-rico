dc095895b5c242d065df074936cc461724836336
María Casasola <marcascal2@alum.us.es>
2020-03-24 18:19:36 +0100

    Optimizado lo máximo posible los tests de Servicio actualmente testeados, faltan métodos por testear

--- src/test/java/org/springframework/samples/acmerico/service/TransferApplicationsTest.java
+++ src/test/java/org/springframework/samples/acmerico/service/TransferApplicationsTest.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,19 +24,18 @@ public class TransferApplicationsTest {
 	@Autowired
 	private TransferAppService transferAppService;
 	
-	private static BankAccount bankAccount = new BankAccount();
-	private static Client client = new Client();
-	private static User user = new User();
+	BankAccount bankAccount = new BankAccount();
+	Client client = new Client();
+	User user = new User();
 	
-	@BeforeAll
-	static void populateUser() {
+	@BeforeEach
+	private void populateData() {
+		TransferApplication transferApp = new TransferApplication();
+		
 		user.setUsername("userPrueba");
 		user.setPassword("userPrueba");
 		user.setEnabled(true);
-	}
-	
-	@BeforeAll
-	static void populateClient(){
+		
 		client.setFirstName("Germán");
 		client.setLastName("Márquez Trujillo");
 		client.setAddress("C/ Marques de Aracena, 37");
@@ -50,34 +49,31 @@ public class TransferApplicationsTest {
 		client.setUser(user);
 		client.setBankAccounts(new ArrayList<BankAccount>());
 		client.getBankAccounts().add(bankAccount);
-	}
-	
-	@BeforeAll
-	static void populateBankAccount(){
+		
 		bankAccount.setAccountNumber("ES23 2323 2323 2323 2323");
 		bankAccount.setAmount(100000.0);
 		bankAccount.setCreatedAt(LocalDateTime.parse("2017-10-30T12:30:00"));
 		bankAccount.setAlias("Viajes");
 		bankAccount.setClient(client);
-	}
-	
-	@Test
-	public void testCountTransferApplications() {
-		Collection<TransferApplication> transferApps = this.transferAppService.findAllTransfersApplications();
-		assertThat(transferApps.size()).isEqualTo(19);
-	}
-
-	@Test
-	public void testCountTransferApplicationsAfterCreating() {
-		TransferApplication transferApp = new TransferApplication();
+		
 		transferApp.setStatus("PENDING");
 		transferApp.setAmount(200.00);
 		transferApp.setAccount_number_destination("ES24 2323 2323 2323 2323");
 		transferApp.setBankAccount(bankAccount);
 		transferApp.setClient(client);
+		
 		this.transferAppService.save(transferApp);
+	}
+
+	@Test
+	public void testCountTransferApplicationsAfterCreating() {
 		Collection<TransferApplication> transferApps = this.transferAppService.findAllTransfersApplications();
 		assertThat(transferApps.size()).isEqualTo(20);
 	}
 	
+	@Test
+	public void testFindTransferApplicationsByClient() {
+		Collection<TransferApplication> transferApps = this.transferAppService.findAllTransfersApplicationsByClient(client);
+		assertThat(transferApps.size()).isEqualTo(1);
+	}
 }
