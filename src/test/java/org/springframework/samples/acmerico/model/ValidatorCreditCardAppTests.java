ba96720b72359983bbb5fe6ba242369dc5530d44
María Casasola <marcascal2@alum.us.es>
2020-03-25 18:54:55 +0100

    Update ValidatorCreditCardAppTests.java
    Añado test de validacion de modelo de CreditCardApplication

--- src/test/java/org/springframework/samples/acmerico/model/ValidatorCreditCardAppTests.java
+++ src/test/java/org/springframework/samples/acmerico/model/ValidatorCreditCardAppTests.java
@@ -68,21 +68,21 @@ public class ValidatorCreditCardAppTests {
 		assertThat(violation.getMessage()).isEqualTo("must not be blank");
 	}
 	
-//	@Test
-//	void shouldNotValidateWhenStatusIncorrect() {
-//
-//		LocaleContextHolder.setLocale(Locale.ENGLISH);
-//
-//		CreditCardApplication ccApp = new CreditCardApplication();
-//		ccApp.setClient(client);
-//		ccApp.setBankAccount(bankAccount);
-//		ccApp.setStatus("cualquier cosa");
-//
-//		Set<ConstraintViolation<CreditCardApplication>> constraintViolations = validator.validate(ccApp);
-//
-//		ConstraintViolation<CreditCardApplication> violation = constraintViolations.iterator().next();
-//		assertThat(violation.getMessage()).isEqualTo("Card application status only can be ACCEPTED, REJECTED or PENDING");
-//	}
+	@Test
+	void shouldNotValidateWhenStatusIncorrect() {
+
+		LocaleContextHolder.setLocale(Locale.ENGLISH);
+
+		CreditCardApplication ccApp = new CreditCardApplication();
+		ccApp.setClient(client);
+		ccApp.setBankAccount(bankAccount);
+		ccApp.setStatus("cualquier cosa");
+
+		Set<ConstraintViolation<CreditCardApplication>> constraintViolations = validator.validate(ccApp);
+
+		ConstraintViolation<CreditCardApplication> violation = constraintViolations.iterator().next();
+		assertThat(violation.getMessage()).isEqualTo("Card application status only can be ACCEPTED, REJECTED or PENDING");
+	}
 
 	@Test
 	void shouldNotValidateWhenClientEmpty() {
