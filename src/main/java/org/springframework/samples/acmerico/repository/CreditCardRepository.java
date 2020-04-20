d51d70f0fdca7a83e8a4819701be408de664d7c1
GermanMT <antonio-german-1@hotmail.com>
2020-04-20 18:13:26 +0200

    CreditCardGenerator

--- src/main/java/org/springframework/samples/acmerico/repository/CreditCardRepository.java
+++ src/main/java/org/springframework/samples/acmerico/repository/CreditCardRepository.java
@@ -1,11 +1,14 @@
 package org.springframework.samples.acmerico.repository;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.repository.query.Param;
 import org.springframework.samples.acmerico.model.CreditCard;
 
 public interface CreditCardRepository {
 
 	CreditCard findById(int id) throws DataAccessException;
+	
+	CreditCard findByNumber(@Param("number") String number) throws DataAccessException;
 
 	void save(CreditCard cc) throws DataAccessException;
 
