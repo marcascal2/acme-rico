9ab23c5bc192f942ae9b6cb91e59bdf8353ff479
GermanMT <antonio-german-1@hotmail.com>
2020-04-22 14:11:34 +0200

    Caso negativo

--- src/test/java/org/springframework/samples/acmerico/ui/BankAccountInfoUITest.java
+++ src/test/java/org/springframework/samples/acmerico/ui/BankAccountInfoUITest.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.acmerico.ui;
 
 import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.Assert.*;
 import org.openqa.selenium.*;
@@ -21,53 +23,62 @@ public class BankAccountInfoUITest {
 
   @BeforeEach
   public void setUp() throws Exception {
-	String pathToGeckoDriver = "C:\\Users\\Anton\\Desktop\\Workspace-DP2";
-	System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe"); 
-    driver = new FirefoxDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  String pathToGeckoDriver = "C:\\Users\\Anton\\Desktop\\Workspace-DP2";
+	  System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe"); 
+      driver = new FirefoxDriver();
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
   public void testBankAccountInfoUIPositive() throws Exception {
-	    driver.get("http://localhost:" + port + "/");
-	    driver.findElement(By.id("login-button")).click();
-	    driver.findElement(By.id("username")).click();
-	    driver.findElement(By.id("username")).clear();
-	    driver.findElement(By.id("username")).sendKeys("client1");
-	    driver.findElement(By.id("password")).click();
-	    driver.findElement(By.id("password")).clear();
-	    driver.findElement(By.id("password")).sendKeys("client1");
-	    driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
-	    driver.findElement(By.linkText("Bank Accounts")).click();
-	    driver.findElement(By.id("dropdown-clients")).click();
-	    driver.findElement(By.id("my-accounts")).click();
-	    driver.findElement(By.linkText("ES23 0025 0148 1259 1424")).click();
-	    assertEquals("ES23 0025 0148 1259 1424", driver.findElement(By.id("accountNumber")).getAttribute("value"));
-	    assertEquals("2567.34", driver.findElement(By.id("amount")).getAttribute("value"));
-	    assertEquals("Viajes", driver.findElement(By.id("alias")).getAttribute("value"));
+	  driver.get("http://localhost:" + port + "/");
+	  driver.findElement(By.id("login-button")).click();
+	  driver.findElement(By.id("username")).click();
+	  driver.findElement(By.id("username")).clear();
+	  driver.findElement(By.id("username")).sendKeys("client1");
+	  driver.findElement(By.id("password")).click();
+	  driver.findElement(By.id("password")).clear();
+	  driver.findElement(By.id("password")).sendKeys("client1");
+	  driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
+	  driver.findElement(By.linkText("Bank Accounts")).click();
+	  driver.findElement(By.id("dropdown-clients")).click();
+	  driver.findElement(By.id("my-accounts")).click();
+	  driver.findElement(By.linkText("ES23 0025 0148 1259 1424")).click();
+	  assertEquals("ES23 0025 0148 1259 1424", driver.findElement(By.id("accountNumber")).getAttribute("value"));
+	  assertEquals("2567.34", driver.findElement(By.id("amount")).getAttribute("value"));
+	  assertEquals("Viajes", driver.findElement(By.id("alias")).getAttribute("value"));
   }
   
   @Test
   public void testBankAccountInfoUINegative() throws Exception {
-    driver.get("http://localhost:" + port + "/");
-    driver.findElement(By.id("login-button")).click();
-    driver.findElement(By.id("username")).click();
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("worker1");
-    driver.findElement(By.id("password")).click();
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("worker1");
-    driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
-    assertEquals(false, isElementPresent(By.linkText("Bank Accounts")));
+	  driver.get("http://localhost:" + port + "/");
+      driver.findElement(By.id("login-button")).click();
+      driver.findElement(By.id("username")).click();
+      driver.findElement(By.id("username")).clear();
+      driver.findElement(By.id("username")).sendKeys("worker1");
+      driver.findElement(By.id("password")).click();
+      driver.findElement(By.id("password")).clear();
+      driver.findElement(By.id("password")).sendKeys("worker1");
+      driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
+      assertEquals(false, isElementPresent(By.linkText("Bank Accounts")));
   }
 
   @AfterEach
   public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
+	  driver.quit();
+      String verificationErrorString = verificationErrors.toString();
+      if (!"".equals(verificationErrorString)) {
+    	  fail(verificationErrorString);
+      }
+  }
+  
+  private boolean isElementPresent(By by) {
+	  try {
+		  driver.findElement(by);
+	      return true;
+	  } catch (NoSuchElementException e) {
+	      return false;
+	  }
   }
 
 }
