273b6f3a2526538f6663ac49cdc76fe1059818a3
GermanMT <antonio-german-1@hotmail.com>
2020-04-22 13:47:24 +0200

    test bien

--- src/test/java/org/springframework/samples/acmerico/ui/BankAccountInfoUITest.java
+++ src/test/java/org/springframework/samples/acmerico/ui/BankAccountInfoUITest.java
@@ -1,32 +1,35 @@
 package org.springframework.samples.acmerico.ui;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BankAccountInfoUITest {
   private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
+  
+  @LocalServerPort
+  private int port;
 
   @BeforeEach
   public void setUp() throws Exception {
 	String pathToGeckoDriver = "C:\\Users\\Anton\\Desktop\\Workspace-DP2";
 	System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe"); 
     driver = new FirefoxDriver();
-    baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
   public void testBankAccountInfoUI() throws Exception {
-	    driver.get("http://localhost:8080/");
+	    driver.get("http://localhost:"+port+ "/");
 	    driver.findElement(By.id("login-button")).click();
 	    driver.findElement(By.id("username")).click();
 	    driver.findElement(By.id("username")).clear();
@@ -34,10 +37,11 @@ public class BankAccountInfoUITest {
 	    driver.findElement(By.id("password")).click();
 	    driver.findElement(By.id("password")).clear();
 	    driver.findElement(By.id("password")).sendKeys("client1");
-	    driver.findElement(By.xpath("//button[@type='submit']")).click();
-	    driver.findElement(By.xpath("//li[@id='dropdown-clients']/a")).click();
-	    driver.findElement(By.xpath("//li[@id='dropdown-clients']/div/a")).click();
-	    driver.findElement(By.xpath("//table[@id='accountsTable']/tbody/tr/td/a")).click();
+	    driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
+	    driver.findElement(By.linkText("Bank Accounts")).click();
+	    driver.findElement(By.id("dropdown-clients")).click();
+	    driver.findElement(By.id("my-accounts")).click();
+	    driver.findElement(By.linkText("ES23 0025 0148 1259 1424")).click();
 	    assertEquals("ES23 0025 0148 1259 1424", driver.findElement(By.id("accountNumber")).getAttribute("value"));
 	    assertEquals("2567.34", driver.findElement(By.id("amount")).getAttribute("value"));
 	    assertEquals("Viajes", driver.findElement(By.id("alias")).getAttribute("value"));
@@ -52,36 +56,4 @@ public class BankAccountInfoUITest {
     }
   }
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
