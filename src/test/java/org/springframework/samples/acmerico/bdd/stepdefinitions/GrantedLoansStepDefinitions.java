6e10e12c7c653b9434a3e8f6948099972c41ff6c
GermanMT <antonio-german-1@hotmail.com>
2020-04-23 16:35:06 +0200

    Cucumber Test

---
+++ src/test/java/org/springframework/samples/acmerico/bdd/stepdefinitions/GrantedLoansStepDefinitions.java
@@ -0,0 +1,54 @@
+package org.springframework.samples.acmerico.bdd.stepdefinitions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.springframework.boot.web.server.LocalServerPort;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GrantedLoansStepDefinitions extends AbstractStep {
+	
+	@LocalServerPort
+    private int port;
+	
+	private StringBuffer verificationErrors = new StringBuffer();
+	
+	 @Given("I am not logged in the system as director")
+	 public void IamNotLogged() throws Exception {
+		 getDriver().get("http://localhost:" + port + "/login");
+	 }
+	 
+	 @When("I tray to login as a director and see the granted loans")
+	 public void ItryToLogin() throws Exception {
+		 getDriver().get("http://localhost:8080/");
+		 getDriver().findElement(By.id("login-button")).click();
+		 getDriver().findElement(By.id("username")).click();
+		 getDriver().findElement(By.id("username")).clear();
+		 getDriver().findElement(By.id("username")).sendKeys("director1");
+		 getDriver().findElement(By.id("password")).click();
+		 getDriver().findElement(By.id("password")).clear();
+		 getDriver().findElement(By.id("password")).sendKeys("director1");
+		 getDriver().findElement(By.id("password")).sendKeys(Keys.ENTER);
+		 getDriver().findElement(By.linkText("Loans")).click();
+		 getDriver().findElement(By.id("loans-dropdown")).click();
+		 getDriver().findElement(By.id("see-loans")).click();
+	 }
+	 
+	 @Then("The list of granted loans is shown")
+	 public void IsLoggedIn() throws Exception {
+		 try {
+			 assertEquals("Préstamo de estudios", getDriver().findElement(By.linkText("Préstamo de estudios")).getText());
+		 } catch (Error e) {
+		     verificationErrors.append(e.toString());
+		 }
+		 
+		 try {
+		     assertEquals("Préstamo hipotecario", getDriver().findElement(By.linkText("Préstamo hipotecario")).getText());
+		 } catch (Error e) {
+		     verificationErrors.append(e.toString());
+		 }
+	 }
+
+}
