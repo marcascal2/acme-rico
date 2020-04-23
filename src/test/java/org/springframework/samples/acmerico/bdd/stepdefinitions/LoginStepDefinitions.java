08f4610eb97aba96a271ea28478bcef3381b2832
Moises Calzado <moisescalzado1227@gmail.com>
2020-04-23 12:40:14 +0200

    Cambios para que funcionen UI TEST CUCUMBER

--- src/test/java/org/springframework/samples/acmerico/bdd/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/org/springframework/samples/acmerico/bdd/stepdefinitions/LoginStepDefinitions.java
@@ -3,6 +3,8 @@ package org.springframework.samples.acmerico.bdd.stepdefinitions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
@@ -33,7 +35,9 @@ public class LoginStepDefinitions extends AbstractStep {
 
     @Then("Client name is shown as the current user")
     public void IsLoggedIn() throws Exception {
-        assertEquals("client1", getDriver().findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).getText());
+        WebDriverWait wait = new WebDriverWait(getDriver(), 20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//strong[@id='username']")));
+        assertEquals("CLIENT1", getDriver().findElement(By.xpath("//strong[@id='username']")).getText());
         stopDriver();
     }
 
