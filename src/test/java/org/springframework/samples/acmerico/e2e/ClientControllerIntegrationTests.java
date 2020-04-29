1462166a32b0277172405e6525236e12117e7afb
María Casasola <marcascal2@alum.us.es>
2020-04-29 17:49:41 +0200

    Corrijo el tests de integracion de ejemplo, ahora sí funciona correctamente

--- src/test/java/org/springframework/samples/acmerico/e2e/ClientControllerIntegrationTests.java
+++ src/test/java/org/springframework/samples/acmerico/e2e/ClientControllerIntegrationTests.java
@@ -2,40 +2,35 @@ package org.springframework.samples.acmerico.e2e;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.samples.acmerico.service.ClientService;
-import org.springframework.samples.acmerico.service.UserService;
 import org.springframework.samples.acmerico.web.ClientController;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@Transactional
+//@TestPropertySource(locations = "classpath:application-mysql.properties")
 class ClientControllerIntegrationTests {
 
 	private static final Integer TEST_CLIENT_ID = 1;
 
 	@Autowired
 	private ClientController clientController;
-
-	@MockBean
-	private ClientService clientService;
-
-	@MockBean
-	private UserService UserService;
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -50,19 +45,12 @@ class ClientControllerIntegrationTests {
 
 	@WithMockUser(username = "director1", authorities = { "director" })
 	@Test
-	void testProcessCreationFormSuccess() throws Exception {
-		mockMvc.perform((post("/clients/{clientId}/edit", TEST_CLIENT_ID).with(csrf())
-				.param("firstName", "Client")
-				.param("lastName", "Updated")
-				.param("address", "Gordal")
-				.param("birthDate", "1998/11/27")
-				.param("city", "Sevilla")
-				.param("maritalStatus", "single but whole")
-				.param("salaryPerYear", "300000.")
-				.param("age", "21")
-				.param("job", "student")
-				.param("lastEmployDate", "2010/01/22"))
-				.with(csrf()))
+	void testProcessUpdateFormSuccess() throws Exception {
+		mockMvc.perform((post("/clients/{clientId}/edit", TEST_CLIENT_ID).with(csrf()).param("firstName", "Client")
+				.param("lastName", "Updated").param("address", "Gordal").param("birthDate", "1998/11/27")
+				.param("city", "Sevilla").param("maritalStatus", "single but whole").param("salaryPerYear", "300000")
+				.param("age", "21").param("job", "student").param("lastEmployDate", "2010/01/22"))
+						.param("user.username", "client1").param("user.password", "client1").with(csrf()))
 				.andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/clients/{clientId}"));
 	}
 
