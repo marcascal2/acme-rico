cadc3534c5e1eb639a073deb4e5ac25499b01bba
Javier Ruiz <javruimun@alum.us.es>
2020-05-07 13:00:01 +0200

    Update DashBoardController

--- src/main/java/org/springframework/samples/acmerico/web/DashboardController.java
+++ src/main/java/org/springframework/samples/acmerico/web/DashboardController.java
@@ -15,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DashboardController {
