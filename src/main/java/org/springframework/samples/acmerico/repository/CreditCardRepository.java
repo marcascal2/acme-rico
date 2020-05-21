c5aff3bc9cbb0d632c9da28d2117d2b8c60a901f
Moises Calzado <moisescalzado1227@gmail.com>
2020-05-21 16:45:03 +0200

    Arreglo un test para borrar las credit card por un problema en el repositorio

--- src/main/java/org/springframework/samples/acmerico/repository/CreditCardRepository.java
+++ src/main/java/org/springframework/samples/acmerico/repository/CreditCardRepository.java
@@ -2,19 +2,26 @@ package org.springframework.samples.acmerico.repository;
 
 import java.util.Collection;
 
+import javax.transaction.Transactional;
+
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.samples.acmerico.model.CreditCard;
 
-public interface CreditCardRepository {
+public interface CreditCardRepository extends CrudRepository<CreditCard, Integer> {
 
 	CreditCard findById(int id) throws DataAccessException;
 	
 	CreditCard findByNumber(String number) throws DataAccessException;
 
-	void deleteCreditCard(int id) throws DataAccessException;
-
-	void save(CreditCard creditCard) throws DataAccessException;
-
 	Collection<CreditCard> findAll();
+	
+    @Transactional
+	@Modifying
+	@Query("DELETE FROM CreditCard cc WHERE cc.id =:id")
+	public void deleteCreditCard(@Param("id") int id);
 
 }
