be0f2759033fea98c7926a2def8bd0b56249e2f5
Moises Calzado <moisescalzado1227@gmail.com>
2020-05-26 12:39:30 +0200

    Arreglo error en el dashboard

--- src/main/java/org/springframework/samples/acmerico/repository/DashboardRepository.java
+++ src/main/java/org/springframework/samples/acmerico/repository/DashboardRepository.java
@@ -9,37 +9,37 @@ import org.springframework.samples.acmerico.model.User;
 
 public interface DashboardRepository extends CrudRepository<User, Integer> {
 
-	@Query("SELECT COUNT(loan_application) FROM LoanApplication loan_application WHERE loan_application.status = 'ACCEPTED' AND loan_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(loan_application), 0) FROM LoanApplication loan_application WHERE loan_application.status = 'ACCEPTED' AND loan_application.client LIKE :client")
 	Integer countLoanAppsAccepted(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(loan_application) FROM LoanApplication loan_application WHERE loan_application.status = 'PENDING' AND loan_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(loan_application), 0) FROM LoanApplication loan_application WHERE loan_application.status = 'PENDING' AND loan_application.client LIKE :client")
 	Integer countLoanAppsPending(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(loan_application) FROM LoanApplication loan_application WHERE loan_application.status = 'REJECTED' AND loan_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(loan_application), 0) FROM LoanApplication loan_application WHERE loan_application.status = 'REJECTED' AND loan_application.client LIKE :client")
 	Integer countLoanAppsRejected(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(credit_card_application) FROM CreditCardApplication credit_card_application WHERE credit_card_application.status = 'ACCEPTED' AND credit_card_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(credit_card_application), 0) FROM CreditCardApplication credit_card_application WHERE credit_card_application.status = 'ACCEPTED' AND credit_card_application.client LIKE :client")
 	Integer countCreditCardAppsAccepted(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(credit_card_application) FROM CreditCardApplication credit_card_application WHERE credit_card_application.status = 'PENDING' AND credit_card_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(credit_card_application), 0) FROM CreditCardApplication credit_card_application WHERE credit_card_application.status = 'PENDING' AND credit_card_application.client LIKE :client")
 	Integer countCreditCardAppsPending(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(credit_card_application) FROM CreditCardApplication credit_card_application WHERE credit_card_application.status = 'REJECTED' AND credit_card_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(credit_card_application), 0) FROM CreditCardApplication credit_card_application WHERE credit_card_application.status = 'REJECTED' AND credit_card_application.client LIKE :client")
 	Integer countCreditCardAppsRejected(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(transfer_application) FROM TransferApplication transfer_application WHERE transfer_application.status = 'ACCEPTED' AND transfer_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(transfer_application), 0) FROM TransferApplication transfer_application WHERE transfer_application.status = 'ACCEPTED' AND transfer_application.client LIKE :client")
 	Integer countTransferAppsAccepted(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(transfer_application) FROM TransferApplication transfer_application WHERE transfer_application.status = 'PENDING' AND transfer_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(transfer_application), 0) FROM TransferApplication transfer_application WHERE transfer_application.status = 'PENDING' AND transfer_application.client LIKE :client")
 	Integer countTransferAppsPending(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT COUNT(transfer_application) FROM TransferApplication transfer_application WHERE transfer_application.status = 'REJECTED' AND transfer_application.client LIKE :client")
+	@Query("SELECT COALESCE(COUNT(transfer_application), 0) FROM TransferApplication transfer_application WHERE transfer_application.status = 'REJECTED' AND transfer_application.client LIKE :client")
 	Integer countTransferAppsRejected(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT SUM(amount) FROM Debt debt WHERE debt.client LIKE :client")
+	@Query("SELECT COALESCE(SUM(amount), 0) FROM Debt debt WHERE debt.client LIKE :client")
 	Integer countMoneyToDebt(@Param("client") Client client) throws DataAccessException;
 	
-	@Query("SELECT SUM(amount) FROM BankAccount bank_account WHERE bank_account.client LIKE :client")
+	@Query("SELECT COALESCE(SUM(amount), 0) FROM BankAccount bank_account WHERE bank_account.client LIKE :client")
 	Integer countMoneyInBankAccounts(@Param("client") Client client) throws DataAccessException;
 	
 }
--- src/main/java/org/springframework/samples/acmerico/web/DashboardController.java
+++ src/main/java/org/springframework/samples/acmerico/web/DashboardController.java
@@ -41,17 +41,18 @@ public class DashboardController {
 		model.addAttribute("transferAppsStatus", applicationsStatus.get("transferAppsStatus"));
 		
 		Map<String, List<Object>> amountsToPay = this.dashboardService.amountsToPay(username);
+		
 
 		model.addAttribute("months", amountsToPay.get("months"));
 		model.addAttribute("amountsToPay", amountsToPay.get("amountsToPay"));
-		
+
 		Map<String, Double> moneyDebtPie = this.dashboardService.moneyDebtPie(username);
-		
+
 		model.addAttribute("moneyToDebt", moneyDebtPie.get("moneyToDebt"));
 		model.addAttribute("moneyInBankAccounts", moneyDebtPie.get("moneyInBankAccounts"));
 		
 		Map<String, List<Object>> myMoneyPie = this.dashboardService.myMoneyPie(username);
-		
+
 		model.addAttribute("bankAccountAmounts", myMoneyPie.get("bankAccountAmounts"));
 		model.addAttribute("alias", myMoneyPie.get("alias"));
 		
