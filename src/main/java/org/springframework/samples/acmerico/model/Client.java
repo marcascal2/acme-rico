a35535a422bcbbfb9e23b9d85a96dbe01763bfca
GermanMT <antonio-german-1@hotmail.com>
2020-06-02 15:02:47 +0200

    Delete warning

--- src/main/java/org/springframework/samples/acmerico/model/Client.java
+++ src/main/java/org/springframework/samples/acmerico/model/Client.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
