3b2477b9a251c6e45afc33cd80624a65afd90202
María Casasola <marcascal2@alum.us.es>
2020-06-02 14:42:19 +0200

    Comento linea de driver

--- src/test/java/org/springframework/samples/acmerico/ui/AcceptAndRejectTransferAppUITest.java
+++ src/test/java/org/springframework/samples/acmerico/ui/AcceptAndRejectTransferAppUITest.java
@@ -23,7 +23,7 @@ public class AcceptAndRejectTransferAppUITest {
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.gecko.driver", System.getenv("webdriver.gecko.driver"));
+//		System.setProperty("webdriver.gecko.driver", System.getenv("webdriver.gecko.driver"));
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
